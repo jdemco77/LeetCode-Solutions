ab3336852cc2e822ef0f87c3ac718fbfe68ea943
James Demarco <69318902+jdemco77@users.noreply.github.com>
2022-02-14 11:25:05 -0500

    Add files via upload

---
+++ ShuffleArray.java
@@ -0,0 +1,23 @@
+package leetCode;
+
+//	1470. Shuffle the Array
+//	https://leetcode.com/problems/shuffle-the-array/
+public class ShuffleArray {
+
+	public int[] shuffle(int[] nums, int n) {
+        int[] ret=new int[nums.length];
+        int i=0;
+        
+        for(int j=0;j<nums.length;j++){
+            if(j%2==0){
+                ret[j]=nums[i];
+                i++;
+            }
+            if(j%2==1){
+                ret[j]=nums[n];
+                n++;
+            }
+        }
+        return ret;
+    }
+}
